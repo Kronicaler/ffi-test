34a569eacc651e486af087b366f9147e026259ed
TheKroni <68446074+TheKroni@users.noreply.github.com>
2023-09-01 05:05:06 +0200

    first steps to cross compilation

--- build.rs
+++ build.rs
@@ -1,7 +1,19 @@
+use std::env;
+
 fn main() -> miette::Result<()> {
     let path = std::path::PathBuf::from("src");
-    let mut b = autocxx_build::Builder::new("src/main.rs", [&path]).build()?;
-    b.flag_if_supported("-std=c++20")
+    env::set_var("CROSS_COMPILE ", "1");
+
+    let mut b = autocxx_build::Builder::new("src/main.rs", [&path])
+        .extra_clang_args(&["-std=c++20", "-stdlib=libc++", "--x86_64-pc-windows-gnu"])
+        .build()?;
+
+    b.cpp(true)
+        .std("c++20")
+        .flag_if_supported("-std=c++20")
+        .flag_if_supported("-stdlib=libc++")
+        .flag_if_supported("--target=x86_64-pc-windows-gnu")
+        .target("x86_64-pc-windows-gnu")
         .file("src/cpp/input.cpp")
         .compile("autocxx-demo");
 
